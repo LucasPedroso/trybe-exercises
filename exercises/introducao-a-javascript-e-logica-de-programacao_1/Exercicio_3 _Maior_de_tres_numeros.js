fe214ce4f46175832cd152bf75a64ff8cbc673a7
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-02-25 09:47:17 -0300

    Adicionado Exercicio 3 - Maior de tres numeros.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio_3 _Maior_de_tres_numeros.js
@@ -0,0 +1,11 @@
+let num_1 = 5;
+let num_2 = 2;
+let num_3 = 11;
+
+if (num_1 > num_2 && num_1 > num_3) {
+  console.log(`O maior número é ${num_1}!`);
+} else if (num_2 > num_1 && num_2 > num_3) {
+  console.log(`O maior número é ${num_2}!`);
+} else if (num_3 > num_1 && num_3 > num_2) {
+  console.log(`O maior número é ${num_3}!`);
+}
