82914512a64f039f04c923b20f2e0975b2fda944
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-02-25 09:37:05 -0300

    Adicionado Exercicio 1 - Operacoes Arimeticas Basicas.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Exercicio_1_Operacoes_Aritimeticas_Basicas.js
@@ -0,0 +1,14 @@
+let num_1 = 5;
+let num_2 = 2;
+
+let adicao = num_1 + num_2;
+let subtracao = num_1 - num_2;
+let multiplicacao = num_1 * num_2;
+let divisao = num_1 / num_2;
+let modulo = num_1 % num_2;
+
+console.log('Adição = ' + adicao);
+console.log(`Subtracao = ${subtracao}`);
+console.log(`Multiplicacao = ${multiplicacao}`);
+console.log(`Divisão = ${divisao}`);
+console.log(`Módulo = ${modulo}`);
