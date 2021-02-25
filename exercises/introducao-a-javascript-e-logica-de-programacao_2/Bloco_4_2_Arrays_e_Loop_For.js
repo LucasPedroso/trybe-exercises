f6cea6c5ca4e30946516a6ae6972b740e7a06605
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-02-25 19:38:15 -0300

    Bloco 4.2 Arrays e Loop For Exercicio 4 e refatorado Ex 3(variavel mediaNumbers)

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/Bloco_4_2_Arrays_e_Loop_For.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Bloco_4_2_Arrays_e_Loop_For.js
@@ -24,4 +24,17 @@ console.log(somaNumbers);
 /* Para o terceiro exercício, calcule e imprima a média aritmética 
 dos valores contidos no array; */
 
-console.log(somaNumbers / numbers.length);
+let mediaNumbers = somaNumbers / numbers.length;
+console.log(mediaNumbers);
+
+
+/* Com o mesmo código do exercício anterior, caso valor final seja maior que 20, 
+imprima a mensagem: "valor maior que 20". Caso não seja, imprima a 
+mensagem: "valor menor ou igual a 20"; */
+
+if (mediaNumbers) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
