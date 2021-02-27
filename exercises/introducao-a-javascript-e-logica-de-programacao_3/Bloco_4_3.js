f40c062a2ce2677cf51100bb3e6a8c2ca434a238
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-02-26 21:38:28 -0300

    Bloco 4.3 - Lógica de Programação e Algorítimos Exercício 2 e 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/Bloco_4_3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/Bloco_4_3.js
@@ -1,14 +1,41 @@
-const n = 2;
+const n = 5;
 ast = '*';
 
+/* Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, 
+seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . */
 if (n > 1) {
-  for (let index = 1; index < n; index++) {
+  for (let index = 1; index < n; index += 1) {
     ast += '*';
   }
-  for (let index = 0; index < n; index++) {
+  for (let index = 0; index < n; index += 1) {
     console.log(ast);
   }
 } else {
   console.log('Número inválido. Digite um número maior que 1.');
 }
 
+/* Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo 
+com 5 asteriscos de base. */
+console.log('===================================');
+ast = '*';
+for (let index = 0; index < n; index += 1) {
+  console.log(ast);
+  ast += '*';
+}
+
+/* Agora inverta o lado do triângulo. */
+console.log('===================================');
+
+ast = '*';
+let position = n;
+for (let index = 1; index < n; index += 1) {  
+  let space = ' ';
+
+  for (let j = 1; j < position; j++) {
+    space += ' ';
+    console.log(j);
+  }
+  position--;
+  space += ast;
+  console.log(space);
+}
