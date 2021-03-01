661894bc01e50a70f9e2f5daf95abc6bebffbdc7
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 20:39:28 -0300

    Bloco 4.4 - Introdução à JavaScript e Lógica de Programação Exercício 2 Add moreRepeatedNumber(output) and highestCountInArrayOfObject(output)

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
@@ -28,6 +28,7 @@ console.log(lowerValue(arrayTest));
 /* Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
 Valor esperado no retorno da função: Fernanda . */
+//Referência MDN e StackOverflow
 arrayTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 function highestString(array) {
   return array.reduce((a, b) => {
@@ -39,3 +40,36 @@ function highestString(array) {
                                 });
 }
 console.log('highString ' + highestString(arrayTest));
+
+/* Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+Valor esperado no retorno da função: 2 . */
+//Referência https://www.codegrepper.com/code-examples/javascript/how+to+get+the+number+repetetions+in+an+array+javascript
+arrayTest = [2, 3, 2, 5, 8, 2, 3];
+function moreRepeatedNumber(array) {
+  let output =  array.reduce((b,c)=>(
+                              (b[b.findIndex(d => d.el === c)]
+                              ||b[b.push({el:c,count:0})-1])
+                              .count++,b
+                              ),[]);
+
+  return highestCountInArrayOfObject(output);
+}
+console.log(moreRepeatedNumber(arrayTest));
+
+/* //Não apagado para estudos posteriores
+uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
+var count = [];
+arrayTest.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+ */
+
+// Referência ajuda de @Bernardo Salgueiro
+function highestCountInArrayOfObject(output) {
+  let result = output.reduce((a, b) => {
+    if (b.count > a.count) {
+      return b;
+    }
+    return a;
+  });
+  return result.el;
+}
