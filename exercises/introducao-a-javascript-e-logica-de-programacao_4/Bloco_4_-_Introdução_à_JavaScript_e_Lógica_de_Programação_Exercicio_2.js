f2fa2c3dfb91b9fb915c2255d7ce1e18bf50dda4
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 16:13:28 -0300

    Bloco 4 - Introdução à JavaScript e Lógica de Programação Exercício 2 Add lowerValue(array)

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
@@ -16,3 +16,14 @@ function highestValue(array) {
   return array.indexOf( array.reduce((a, b) => Math.max(a, b)) );
 }
 console.log(highestValue(arrayTest));
+
+/* Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+Valor esperado no retorno da função: 6 . */
+
+arrayTest = [2, 4, 6, 7, 10, 0, -3];
+function lowerValue(array) {
+  //let output = array;
+  return array.indexOf( array.reduce((a, b) => Math.min(a, b)) );
+}
+console.log(lowerValue(arrayTest));
