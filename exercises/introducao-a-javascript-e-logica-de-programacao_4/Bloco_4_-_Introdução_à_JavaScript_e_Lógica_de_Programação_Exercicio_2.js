1c9f4b73c9462640424ee39574bb0e3c6084231b
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 16:11:08 -0300

    Bloco 4 - Introdução à JavaScript e Lógica de Programação Exercício 2 Add highestValue(array)

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
@@ -4,3 +4,15 @@ function verifyPalindrome(string){
 }
 
 console.log(verifyPalindrome('arara'));
+
+/* Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+Array de teste: [2, 3, 6, 7, 10, 1]; .
+Valor esperado no retorno da função: 4 . */
+
+//Referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+let arrayTest = [2, 3, 6, 7, 10, 1];
+function highestValue(array) {
+  //let output = array;
+  return array.indexOf( array.reduce((a, b) => Math.max(a, b)) );
+}
+console.log(highestValue(arrayTest));
