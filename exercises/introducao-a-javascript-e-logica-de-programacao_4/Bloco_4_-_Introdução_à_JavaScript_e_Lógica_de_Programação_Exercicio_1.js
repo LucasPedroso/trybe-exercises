ca5467dd8f3fa30c8f90dffbb841f58808786df5
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 15:32:21 -0300

    Bloco 4 - Introdução à JavaScript e Lógica de Programação Exercício 1 adicionado info["recorrente"]

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_1.js
@@ -1,11 +1,22 @@
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
+/* 
+Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+Valor esperado no console: Bem-vinda, Margarida */
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 console.log(`Bem-vinda, ${info.personagem}`);
-/* 
-Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-Valor esperado no console: Bem-vinda, Margarida */
+
+/* Valor esperado no console:
+Copiar
+  {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  }; */
+info['recorrente'] = 'Sim';
+console.log(info);
