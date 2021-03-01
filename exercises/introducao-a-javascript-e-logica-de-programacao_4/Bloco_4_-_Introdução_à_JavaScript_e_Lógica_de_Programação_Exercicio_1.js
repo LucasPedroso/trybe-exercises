70307dcdb80df31aeb508a3b9939d539a2c2610a
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 15:47:39 -0300

    Bloco 4 - Introdução à JavaScript e Lógica de Programação Exercício 1 adicionado for/in e obj info2

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_1.js
@@ -4,9 +4,9 @@
 Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 Valor esperado no console: Bem-vinda, Margarida */
 let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 console.log(`Bem-vinda, ${info.personagem}`);
 
@@ -42,4 +42,26 @@ Valor esperado no console:
   Sim */
 for (const key in info) {
   console.log(info[key]);
+}
+
+/* Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+Valor esperado no console:
+Copiar
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha! */
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (const key in info, info2) {
+  if (info[key] === info2[key]) {
+    console.log('Ambos recorrentes');
+    continue;
+  }
+  console.log(`${info[key]} e ${info2[key]}`);
 }
