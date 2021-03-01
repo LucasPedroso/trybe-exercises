fbce80dc4e5f0082ec1a66785105a5b47c9733f7
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 16:54:09 -0300

    Bloco 4.4 - Introdução à JavaScript e Lógica de Programação Exercício 2 Add highestString(array)

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
@@ -12,7 +12,6 @@ Valor esperado no retorno da função: 4 . */
 //Referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 let arrayTest = [2, 3, 6, 7, 10, 1];
 function highestValue(array) {
-  //let output = array;
   return array.indexOf( array.reduce((a, b) => Math.max(a, b)) );
 }
 console.log(highestValue(arrayTest));
@@ -20,10 +19,23 @@ console.log(highestValue(arrayTest));
 /* Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
 Valor esperado no retorno da função: 6 . */
-
 arrayTest = [2, 4, 6, 7, 10, 0, -3];
 function lowerValue(array) {
-  //let output = array;
   return array.indexOf( array.reduce((a, b) => Math.min(a, b)) );
 }
 console.log(lowerValue(arrayTest));
+
+/* Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+Valor esperado no retorno da função: Fernanda . */
+arrayTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+function highestString(array) {
+  return array.reduce((a, b) => {
+                                  if (a.length > b.length) { //Evitando o operador ternário
+                                    return a
+                                  } else {
+                                    return b;
+                                  }        
+                                });
+}
+console.log('highString ' + highestString(arrayTest));
