9edf1d15b2a6ef617bf2ce34262148833dd00680
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-01 21:38:19 -0300

    Bloco 4.4 - Introdução à JavaScript e Lógica de Programação Exercício 2 Add verifySameEndWord(word, search)

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Exercicio_2.js
@@ -85,3 +85,19 @@ const sumOfOneToNumber = (number) => {
   return output;
 }
 console.log(sumOfOneToNumber(5));
+
+/* Crie uma função que receba uma string word e outra string ending . 
+Verifique se a string ending é o final da string word . 
+Considere que a string ending sempre será menor que a string word .
+Valor de teste: "trybe" e "be"
+Valor esperado no retorno da função: true
+verificaFimPalavra("trybe", "be") ;
+Retorno esperado: true
+verificaFimPalavra("joaofernando", "fernan") ;
+Retorno esperado: false */
+
+const verifySameEndWord = (word, search) => {
+  if (word.length < search.length) return false;
+  return word.substr(-Math.abs(search.length)) === search;
+}
+console.log(verifySameEndWord('trybe' , 'be'));
