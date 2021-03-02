4793a38a0b4fc424e416b3c61873a8f9126558ba
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-02 11:05:01 -0300

    Bloco 4.4 - Introdução à JavaScript e Lógica de Programação Exercício Bônus apagado console.logs

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Bonus.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/Bloco_4_-_Introdução_à_JavaScript_e_Lógica_de_Programação_Bonus.js
@@ -1,103 +1,67 @@
-function verifyPalindrome(string){
-  const output = string;
-  return string == string.split('').reverse().join('');
+const sum = (a, b) => {
+  return a + b;
 }
 
-console.log(verifyPalindrome('arara'));
-
-/* Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
-Array de teste: [2, 3, 6, 7, 10, 1]; .
-Valor esperado no retorno da função: 4 . */
-
-//Referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
-let arrayTest = [2, 3, 6, 7, 10, 1];
-function highestValue(array) {
-  return array.indexOf( array.reduce((a, b) => Math.max(a, b)) );
+const subtraction = (a, b) => {
+  return b - a;
 }
-console.log(highestValue(arrayTest));
 
-/* Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
-Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
-Valor esperado no retorno da função: 6 . */
-arrayTest = [2, 4, 6, 7, 10, 0, -3];
-function lowerValue(array) {
-  return array.indexOf( array.reduce((a, b) => Math.min(a, b)) );
-}
-console.log(lowerValue(arrayTest));
-
-/* Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
-Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
-Valor esperado no retorno da função: Fernanda . */
-//Referência MDN e StackOverflow
-arrayTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-function highestString(array) {
-  return array.reduce((a, b) => {
-                                  if (a.length > b.length) { //Evitando o operador ternário
-                                    return a
-                                  } else {
-                                    return b;
-                                  }        
-                                });
-}
-console.log('highString ' + highestString(arrayTest));
-
-/* Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
-Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
-Valor esperado no retorno da função: 2 . */
-//Referência https://www.codegrepper.com/code-examples/javascript/how+to+get+the+number+repetetions+in+an+array+javascript
-arrayTest = [2, 3, 2, 5, 8, 2, 3];
-function moreRepeatedNumber(array) {
-  let output =  array.reduce((b,c)=>(
-                              (b[b.findIndex(d => d.el === c)]
-                              ||b[b.push({el:c,count:0})-1])
-                              .count++,b
-                              ),[]);
-
-  return highestCountInArrayOfObject(output);
-}
-console.log(moreRepeatedNumber(arrayTest));
-
-/* //Não apagado para estudos posteriores
-uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
-var count = [];
-arrayTest.forEach(function(i) { count[i] = (count[i]||0) + 1;});
- */
-
-// Referência ajuda de @Bernardo Salgueiro
-function highestCountInArrayOfObject(output) {
-  let result = output.reduce((a, b) => {
-    if (b.count > a.count) {
-      return b;
+//Corrigir está função
+function checkRomanNumeral(arrayNumbers ){
+  let output = [...arrayNumbers];
+  console.log(output);
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] == arrayNumbers[index + 1] &&
+        arrayNumbers[index] == arrayNumbers[index + 2] &&
+        arrayNumbers[index] == arrayNumbers[index + 3]){
+      console.log('Digite um número romano válido.');
+      console.log('Não pode repetir uma letra mais que 3 vezes seguidas.');
+      return false;
     }
-    return a;
-  });
-  return result.el;
-}
-
-/* Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
-Valor de teste: N = 5 .
-Valor esperado no retorno da função: 1+2+3+4+5 = 15 . */
-const sumOfOneToNumber = (number) => {
-  let output = 0;
-  for (let index = 1; index <= number; index += 1) {
-    output += index;
   }
-  return output;
 }
-console.log(sumOfOneToNumber(5));
 
-/* Crie uma função que receba uma string word e outra string ending . 
-Verifique se a string ending é o final da string word . 
-Considere que a string ending sempre será menor que a string word .
-Valor de teste: "trybe" e "be"
-Valor esperado no retorno da função: true
-verificaFimPalavra("trybe", "be") ;
-Retorno esperado: true
-verificaFimPalavra("joaofernando", "fernan") ;
-Retorno esperado: false */
+const romanNumerals = (romanNumerals) => {
+  let result = 0;
+  let count = 0
+  let romanNumeralsObj = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
 
-const verifySameEndWord = (word, search) => {
-  if (word.length < search.length) return false;
-  return word.substr(-Math.abs(search.length)) === search;
+  let array = romanNumerals.split('');
+  
+  let arrayNum = array.map(letter => {
+    for (let index = 0; index < romanNumerals.length; index += 1) {
+      for (const i in romanNumeralsObj) {
+        if (letter[index] === i) {
+          return romanNumeralsObj[i];
+        }
+      }
+    }
+  });
+
+  if (checkRomanNumeral(arrayNum)) {
+    return console.log('Corrigir!');
+  }
+
+  for (let index = 0; index < arrayNum.length; index += 1) {
+    if (arrayNum[index] >= arrayNum[index + 1] && arrayNum[index + 1] >= arrayNum[index + 2]) {
+      result += sum(arrayNum[index], arrayNum[index + 1]);
+      index += 1;
+    } else if (arrayNum[index] < arrayNum[index + 1]){
+      result += subtraction(arrayNum[index], arrayNum[index + 1]);
+      index += 1;
+    } else {
+      result += arrayNum[index];
+    }
+  }
+  console.log(result);
 }
-console.log(verifySameEndWord('trybe' , 'be'));
+
+romanNumerals('MCDLXXXIV'); //1484
