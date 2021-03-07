ecbada6986c065c9746447d93def9c6f74830d34
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-07 20:01:50 -0300

    Bloco 5.3 - JavaScript: DOM, Eventos e Web Storage Exercícios. Exercício 3

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -29,7 +29,7 @@ const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
   17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 let holidays = [24, 25, 31];
 let fridays = [4, 11, 18, 25];
-  
+
 const createDays = (day, holidays, friday) => {
   let days = document.querySelector('#days');
   let tagLi = document.createElement('li');
@@ -61,10 +61,38 @@ Adicione este botão como filho/filha da tag <div> com classe "buttons-container
 
 const createBtn = (text, elParent, className) => {
   let btn = document.createElement('button');
-  btn.value = text;
+  btn.innerText = text;
   if (className) btn.classList.add(className);
   document.querySelector(elParent).appendChild(btn);
 }
 
 createBtn('Feriados', '.buttons-container', 'btn-holiday');
 
+/* Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a 
+cor de fundo dos dias que possuem a classe "holiday" .
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele 
+retorna à configuração inicial com a cor "rgb(238,238,238)" . */
+
+const addEvListener = (elementNode, event, fn) => {
+  if (typeof fn !== 'function') {
+    console.log('O parâmetro fn deve ser uma função.');
+    return;
+  }
+  let node = document.querySelector(elementNode);
+  node.addEventListener(event, fn);
+}
+
+const changeColor = (newColor, oldColor) => {
+  let elements = document.querySelectorAll('.holiday');
+  for (let index = 0; index < elements.length; index += 1) {
+    let el = elements[index];
+
+    if (el.style.backgroundColor === newColor) {
+      el.style.backgroundColor = oldColor;
+    } else {
+      el.style.backgroundColor = newColor;
+    }
+  }
+}
+
+addEvListener('.btn-holiday', 'click', function (e) { changeColor('black', 'rgb(238, 238, 238)') })
