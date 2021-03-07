0bf01a4745e57d0c22f909bfb3f666816af9d788
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-07 17:58:57 -0300

    Bloco 5.3 - JavaScript: DOM, Eventos e Web Storage Exercícios. Exercício 1

---
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,57 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+/* O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. 
+Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas 
+da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam 
+respectivamente Segunda-feira e Terça-feira.
+
+Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . 
+Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . 
+Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . 
+Ex: <li class="day friday">4</li> */
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+  17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let holidays = [24, 25, 31];
+let fridays = [4, 11, 18, 25];
+  
+const createDays = (day, holidays, friday) => {
+  let days = document.querySelector('#days');
+  let tagLi = document.createElement('li');
+  tagLi.innerText = day;
+  tagLi.classList.add('day');
+  if (holidays.length > 0) {
+    for (const h of holidays) {
+      if (day === h) addClass(tagLi, 'holiday');
+    }
+  }
+  if (friday.length > 0) {
+    for (const f of fridays) {
+      if (day === f) addClass(tagLi, 'friday');
+    }
+  }
+  days.appendChild(tagLi);
+}
+
+const addClass = (element, className) => element.classList.add(className);
+
+for (const day of dezDaysList) {
+  createDays(day, holidays, fridays);
+}
+
+
