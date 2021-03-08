1e4b1c646c8a985c087780c2179b23f9e9e7b264
Lucas Eduardo Pedroso <lepsk8@gmail.com>
2021-03-07 21:03:55 -0300

    Bloco 5.3 - JavaScript: DOM, Eventos e Web Storage Exercícios. Exercício 5

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -104,3 +104,21 @@ Adicione a este botão o ID "btn-friday" .
 Adicione este botão como filho/filha da tag <div> com classe "buttons-container" . */
 
 createBtn('Sexta-Feira', '.buttons-container', '#btn-friday')
+
+/* Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto 
+exibido nos dias que são Sexta-feira.
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente 
+ele retorna à configuração inicial exibindo os dias. */
+
+const toggleTextDays = (days, text, elCSS) => {
+  let daysElements = document.querySelectorAll(elCSS);
+
+  for (let index = 0; index < daysElements.length; index += 1) {
+    if (daysElements[index].innerText === text) {
+      daysElements[index].innerText = days[index];
+    } else {
+      daysElements[index].innerText = text;
+    }
+  }
+}
+addEvListener('#btn-friday', 'click', function (e) { toggleTextDays(fridays, 'Sexta-Feira', '.friday') })
